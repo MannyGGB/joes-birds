fa905c59b3b4ecaa9c346e1d83d864468f663d58
Manuel <manugonzalez.tech@gmail.com>
2025-04-02 10:43:36 +0100

    fix: amend vite-env to add types to process

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly NODE_ENV: string;
+  readonly VITE_SERVER_GET_URL: string;
+  readonly VITE_SERVER_POST_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
